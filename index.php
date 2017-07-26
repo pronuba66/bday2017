7f2cce6cafde630fd9ecc066d2ef6f17c8dbbbdd
Joseph Stephen <pronuba66@gmail.com>
2017-07-26 09:58:26 +0530

    https

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 
 
 	<title>Run Difya Run</title>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,600' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,600' rel='stylesheet' type='text/css'>
 	<!--<script src="js/jquery.min.js"></script>-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="js/snap.svg.min.js"></script>
